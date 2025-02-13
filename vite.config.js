2af06ff3fae0f820aecd7f7c37b33d054c98b9cf
Ruslan Smirnov <52673324+trinner01@users.noreply.github.com>
2025-02-13 21:06:04 +0300

    vue deploy files

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/diplomInterview/",
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+// })
+
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/diplomInterview/'
+  : '/'
+}
